f4a07a7093e02fb2b0b1bb29769d8211c559f9cb
AlainaMariaJoe <alainamariajoe33@gmail.com>
2024-12-09 23:51:58 +0530

    sum_digits.py

--- BASICS/sum_digits.py
+++ BASICS/sum_digits.py
@@ -1,8 +1,7 @@
 def sum_of_digits_until_single_digit(num):
-    # Continue summing the digits until a single-digit number is obtained
-    while num >= 10:  # Keep looping as long as the number is 2 or more digits
-        num = sum(int(digit) for digit in str(num))  # Sum the digits of the number
-    return num  # Return the final single-digit result
+    while num >= 10:  
+        num = sum(int(digit) for digit in str(num))  
+    return num  
 
 # Example usage:
 number = 9875
